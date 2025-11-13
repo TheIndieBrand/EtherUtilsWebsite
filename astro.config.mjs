268b7012aed074e2eb65bad7a0ec2fb97ee082fc
devEthan6737 <victorchacongarcia2006@gmail.com>
2025-11-13 19:43:43 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+export default defineConfig({
+    output: 'server',
+    adapter: node({
+        mode: 'standalone',
+    }),
+    devToolbar: {
+        enabled: false
+    },
+    server: {
+        host: "0.0.0.0", 
+        port: 20227,
+    }
+});
